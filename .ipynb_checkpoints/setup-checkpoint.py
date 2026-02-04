ab82a056b70fb71c4df6224e21147ded513aa6cd
Diane Salim <dsalim@rusty1.cm.cluster>
2026-02-04 13:49:19 -0500

    updated name

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="symr",  # module/package name
+    name="symr_fire2",  # module/package name
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
